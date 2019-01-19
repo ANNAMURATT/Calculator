a9986d750d340872b81039470e471f4fafdd3782
ANNAMURATT <38633696+ANNAMURATT@users.noreply.github.com>
2019-01-19 16:09:00 -0600

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,4 +8,9 @@ public class Calculator {
 //         subtracting numbers
         return num1-num2;
     }
+    
+     public static double multiply(double n1, double n2){
+//         multiplying numbers
+        return n1*n2;
+    }
 }
